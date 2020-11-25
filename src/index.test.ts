8c7d7a0ba3cc7f82c6a92af6feef90a1496f92cb
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2020-11-24 21:11:00 -0800

    base test cases

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,13 +1,78 @@
-import {
-  testfunc,
-  testfunc2
-} from '@src/index'
+import { calculateMME } from '@src/index'
 
+describe('Given opioid drug product calculate morphine equiv', () => {
+  const testCases: any[] = [
+    {
+      name: 'codeine',
+      dose: 15,
+      frequency: 4,
+      expected: { products: [{name: 'codeine', total: 9}], total: 9 }
+    },
+    {
+      name: 'hydrocodone',
+      dose: 5,
+      frequency: 4,
+      expected: { products: [{name: 'hydrocodone', total: 20}], total: 20 }
+    },
+    {
+      name: 'oxycodone',
+      dose: 10,
+      frequency: 2,
+      expected: { products: [{name: 'oxycodone', total: 30}], total: 30 }
+    },
+    {
+      name: 'methadone',
+      dose: 15,
+      frequency: 2,
+      expected: { products: [{name: 'methadone', total: 240}], total: 240 }
+    },
+    {
+      name: 'fentanylTransdermal',
+      dose: 12.5,
+      frequency: 1,
+      expected: { products: [{name: 'fentanylTransdermal', total: 30}], total: 30 }
+    },
+    {
+      name: 'oxymorphone',
+      dose: 10,
+      frequency: 2,
+      expected: { products: [{name: 'oxymorphone', total: 60}], total: 60 }
+    }
+  ];
 
-test("Hello to tests", () => {
-  expect(testfunc()).toBe('hello tests')
+  testCases.forEach(({name, dose, frequency, expected}) => {
+    test(`given ${name}, dose ${dose} and freq ${frequency} expected mme ${expected.total}`, () => {
+      expect(calculateMME([{name, dose, frequency}])).toStrictEqual(expected)
+    })
+  })
+
 })
 
-test("Test testfunc2 with arg", () => {
-  expect(testfunc2('some arg')).toBe('hello tests againsome arg')
+describe('Given product array calculate individual product total + complete total', () => {
+  const testCases: any[] = [
+    {
+      products: [
+        {
+          name: 'hydrocodone',
+          dose: 5,
+          frequency: 4,
+        },
+        {
+          name: 'oxycodone',
+          dose: 10,
+          frequency: 2,
+        },
+      ],
+      expected: {
+        products: [{name: 'hydrocodone', total: 20}, {name: 'oxycodone', total: 30}],
+        total: 50
+      }
+    },
+  ]
+
+  testCases.forEach((testCase) => {
+    test(`given product array return result with total ${testCase.expected.total}`, () => {
+      expect(calculateMME(testCase.products)).toStrictEqual(testCase.expected)
+    })
+  })
 })
---
+++ src/utils.test.ts
@@ -0,0 +1,41 @@
+import { getScaleFactor, createRange } from '@src/utils'
+
+
+describe('Given two numbers create range', () => {
+  const testCases = [
+    { start: 1, end: 10, expected: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] },
+    { start: 4, end: 8, expected: [4, 5, 6, 7, 8] },
+  ]
+
+  testCases.forEach( ({start, end, expected}) => {
+    test(`given start ${start}, end ${end}, return array ${expected}`, () => {
+      expect(createRange(start, end)).toStrictEqual(expected)
+    })
+  })
+})
+
+describe('Given conversion scale and dose return factor', () => {
+  const conversionObj = {
+    scale: {
+      4: [1, 20],
+      8: [21, 40],
+      10: [41, 60],
+      12: [61, 'greater']
+    }
+  }
+
+  const testCases = [
+    { dose: 10, expected: 4 },
+    { dose: 25, expected: 8 },
+    { dose: 50, expected: 10 },
+    { dose: 75, expected: 12 },
+    { dose: 90, expected: 12 },
+  ]
+
+  testCases.forEach(testCase => {
+    test(`Given dose ${testCase.dose} return conversion factor ${testCase.expected} `, () => {
+      expect(getScaleFactor(conversionObj.scale, testCase.dose)).toBe(testCase.expected)
+    })
+  })
+
+})
