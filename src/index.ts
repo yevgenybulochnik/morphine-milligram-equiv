395c2872e11276e600f7ae3407432bb48d14b3f1
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2020-11-24 21:12:10 -0800

    index file with calculatemme function

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,34 @@
-export function testfunc() {
-  return 'hello tests'
+import type { Opioids } from '@src/constants'
+import { conversionDict } from '@src/constants'
+import { getScaleFactor } from '@src/utils'
+
+type ProductInput = {
+  name: keyof typeof Opioids
+  dose: number
+  frequency: number
 }
 
-export function testfunc2(arg: string) {
-  return 'hello tests again' + arg
+export function calculateMME(products: ProductInput[]) {
+  let result = {
+    products: [] as any,
+    total: 0
+  }
+
+  products.map(product => {
+    let conversionObj = { ...conversionDict[product.name] }
+    if (conversionObj.scale) {
+      let dailyDose = product.dose * product.frequency
+      let conversionFactor = getScaleFactor(conversionObj.scale, dailyDose)
+      let productTotal = dailyDose * conversionFactor
+      result.products.push({name: product.name, total: productTotal})
+      result.total += productTotal
+    } else {
+      let { factor } = conversionObj
+      let productTotal = ( product.dose * product.frequency ) * (factor as number)
+      result.products.push({name: product.name, total: productTotal})
+      result.total += productTotal
+    }
+  })
+
+  return result
 }
