cc5b9e87d4292524084575b289aa8d6b335f4dab
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2020-11-24 21:09:20 -0800

    turn off ts diagnostics

--- jest.config.js
+++ jest.config.js
@@ -5,5 +5,10 @@ const { compilerOptions } = require('./tsconfig.json')
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>' })
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>' }),
+  globals: {
+    'ts-jest': {
+      diagnostics: false
+    }
+  }
 };
